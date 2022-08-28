27f6218c90ba5f69a653f08c1f22571957bcdc45
Yosef Brahmantyo Adi Kristanto <kristantoyo@MNCBank.ID>
2022-08-28 13:53:09 +0700

    Slicing a slice

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ type Person struct {
 func main() {
 	var listpersons = []*Person{
 		{name: "bram", usia: 42},
-		{name: "satya", usia: 22},
+		{name: "nugraha", usia: 22},
 		{name: "adi", usia: 32},
 		{name: "frizky", usia: 40},
 		{name: "sigit", usia: 20},
+		{name: "agus", usia: 25},
+		{name: "satrio", usia: 21},
+		{name: "bayu", usia: 24},
+		{name: "putu", usia: 24},
+		{name: "eka", usia: 24},
 	}
 
 	var showlist = func(list []*Person) {
@@ -24,5 +29,11 @@ func main() {
 		}
 	}
 
+	fmt.Println("===============All Datas=================")
 	showlist(listpersons)
+	fmt.Println()
+	fmt.Println("==================Slicing================")
+	var s = listpersons[3:4]
+	showlist(s)
+
 }
